2dcd0eee1614d73b0096c3918f9304d7aaf214d0
Fahim1556 <fahimhasanthebest@gmail.com>
2021-08-17 15:01:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const biodata=['Fahim','Rahim','Karim'];
+const address=['Naogaon','Mohadebpur','Rajshahi'];
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I am React Person</p>
+        <Person Name={biodata[0]} Information={address[0]} ></Person>
+        <Person Name={biodata[1]} Information={address[1]}></Person>
+        <Person Name={biodata[2]} Information={address[2]}></Person> 
       </header>
     </div>
   );
+}
+
+function Person (props) {
+  return (
+    <div style={{border:'2px solid gold',margin:'20px',padding:'10px'}}>
+    <h1>Name: {props.Name}</h1>
+    <p>Address: {props.Information} </p>
+    </div>
+  )
 }
 
 export default App;
